4e74323220c9fdd59bf4907155767480da32a831
Imhachun <60200112028@uin-alauddin.ac.id>
2015-06-28 09:57:57 +0800

    Laporan8

---
+++ src/laporan8/CobaRunTimeExpection.java
@@ -0,0 +1,22 @@
+package laporan8;
+
+public class CobaRunTimeExpection {
+	
+public static void main(String args[]) {
+		
+		try {
+			int a[] = new int[2];
+			System.out.println("Akses elemen ke empat : " + a[3] ) ;
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("IndexOutOfBoundsException terjadi");
+		}
+		System.out.println ("keluar blok eksepsi");
+	}
+
+	public char[] getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
